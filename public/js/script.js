82b780c18b9988f0fc4a454ff616b1ab2f6839ba
Donald APOVO <apovodonald@gmail.com>
2024-07-19 21:55:24 +0100

    make search recursive

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const searchContainer = document.getElementById("search-container");
     const searchInput = document.getElementById("search-input");
     const resultsDiv = document.getElementById("results");
@@ -8,34 +8,58 @@ document.addEventListener("DOMContentLoaded", function () {
     const progressBarInner = document.getElementById("progress-bar-inner");
     const progressPercentage = document.getElementById("progress-percentage");
 
-    searchInput.addEventListener("input", () =>
-        filterResults(searchInput.value)
-    );
-
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        handleFormSubmit(new FormData(form));
+    //TODO Review the research to resolve the speed problem. A little slow now
+    let debounceTimeout;
+    searchInput.addEventListener("input", () => {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => {
+            filterResults(searchInput.value);
+        }, 500);
     });
+    form.addEventListener("submit", handleFormSubmit);
 
     function filterResults(query) {
         const items = resultsDiv.querySelectorAll("li");
         let hasResults = false;
 
-        items.forEach((item) => {
+        function searchItem(item) {
             const name = item.querySelector("span").textContent.toLowerCase();
-            if (name.includes(query.toLowerCase())) {
-                item.style.display = "";
-                item.closest("ul").style.display = ""; // Show parent folders
-                hasResults = true;
+            const isMatch = name.includes(query.toLowerCase());
+
+            // If the item is a folder, search within it
+            const folderContents = item.querySelector(".folder-contents");
+            if (folderContents) {
+                let folderHasResults = false;
+                folderContents.querySelectorAll("li").forEach((subItem) => {
+                    if (searchItem(subItem)) {
+                        folderHasResults = true;
+                    }
+                });
+
+                // Show or hide the folder based on whether it contains matching items
+                item.style.display = folderHasResults || isMatch ? "" : "none";
+                folderContents.style.display = folderHasResults ? "" : "none";
+
+                return folderHasResults || isMatch;
             } else {
-                item.style.display = "none";
+                item.style.display = isMatch ? "" : "none";
+                return isMatch;
+            }
+        }
+
+        items.forEach((item) => {
+            if (searchItem(item)) {
+                hasResults = true;
             }
         });
 
         noResultsDiv.classList.toggle("hidden", hasResults);
     }
 
-    function handleFormSubmit(formData) {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+
         // Show loader and hide results
         loader.classList.remove("hidden");
         resultsDiv.classList.add("hidden");
@@ -75,19 +99,18 @@ document.addEventListener("DOMContentLoaded", function () {
             filterResults(searchInput.value);
 
             // Show search input if results are present
-            if (resultsDiv.innerHTML.trim() !== "") {
-                searchContainer.classList.remove("hidden");
-            }
+            searchContainer.classList.toggle(
+                "hidden",
+                !resultsDiv.innerHTML.trim()
+            );
         }
 
         // Hide loader and show results
         loader.classList.add("hidden");
         resultsDiv.classList.remove("hidden");
 
-        // Add click event listeners to folder icons
+        // Add click event listeners to folder icons and open the first folder by default
         addFolderIconListeners();
-
-        // Open the first folder by default
         openFirstFolder();
     }
 
@@ -101,12 +124,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function addFolderIconListeners() {
         document.querySelectorAll(".folder-icon").forEach((icon) => {
-            icon.addEventListener("click", function () {
+            icon.addEventListener("click", () => {
                 const folderContents =
-                    this.parentElement.querySelector(".folder-contents");
+                    icon.parentElement.querySelector(".folder-contents");
                 folderContents.classList.toggle("hidden");
-                this.classList.toggle("fa-folder");
-                this.classList.toggle("fa-folder-open");
+                icon.classList.toggle("fa-folder");
+                icon.classList.toggle("fa-folder-open");
             });
         });
     }
@@ -114,8 +137,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function openFirstFolder() {
         const firstFolderIcon = document.querySelector(".folder-icon");
         if (firstFolderIcon) {
-            firstFolderIcon.classList.add("fa-folder-open");
-            firstFolderIcon.classList.remove("fa-folder");
+            firstFolderIcon.classList.replace("fa-folder", "fa-folder-open");
             firstFolderIcon.parentElement
                 .querySelector(".folder-contents")
                 .classList.remove("hidden");
@@ -123,37 +145,41 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function renderFolderStructure(items, level = 0) {
-        return `<ul class="no-list-style ml-${level * 4}">
-            ${items
-                .map((item) => {
-                    const isFolder = item.children && item.children.length > 0;
-                    const iconClass = isFolder
-                        ? "folder-icon cursor-pointer"
-                        : "file-icon";
-                    const icon = isFolder
-                        ? `<i class="fas fa-folder ${iconClass}"></i>`
-                        : `<i class="${getFileIconClass(item.name)}"></i>`;
+        return `
+            <ul class="no-list-style ml-${level * 2}">
+                ${items
+                    .map((item) => {
+                        const isFolder =
+                            item.children && item.children.length > 0;
+                        const iconClass = isFolder
+                            ? "folder-icon cursor-pointer"
+                            : "file-icon";
+                        const icon = isFolder
+                            ? `<i class="fas fa-folder ${iconClass}"></i>`
+                            : `<i class="${getFileIconClass(item.name)}"></i>`;
 
-                    const sizeText = item.size ? getSizeText(item.size) : "";
+                        const sizeText = item.size
+                            ? getSizeText(item.size)
+                            : "";
 
-                    return `
-                    <li class="text-gray-700 mb-2">
-                        ${icon}
-                        <span class="ml-2">${item.name}${
-                        sizeText ? ` - ${sizeText}` : ""
-                    }</span>
-                        ${
-                            isFolder
-                                ? `<ul class="folder-contents ml-4 hidden">${renderFolderStructure(
-                                      item.children,
-                                      level + 1
-                                  )}</ul>`
-                                : ""
-                        }
-                    </li>`;
-                })
-                .join("")}
-        </ul>`;
+                        return `
+                        <li class="text-gray-700 mb-2">
+                            ${icon}
+                            <span class="ml-2">${item.name}${
+                            sizeText ? ` - ${sizeText}` : ""
+                        }</span>
+                            ${
+                                isFolder
+                                    ? `<ul class="folder-contents ml-4 hidden">${renderFolderStructure(
+                                          item.children,
+                                          level + 1
+                                      )}</ul>`
+                                    : ""
+                            }
+                        </li>`;
+                    })
+                    .join("")}
+            </ul>`;
     }
 
     function getSizeText(size) {
